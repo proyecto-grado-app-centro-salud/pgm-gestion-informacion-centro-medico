d98dcb66b7edf571a3ad61b658f0c5555b1fcb40
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-25 19:35:06 -0400

    [Add] configuraciones iniciales

---
+++ src/test/java/com/example/microservicio_informacion_centro_medico/MicroservicioInformacionCentroMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_informacion_centro_medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioInformacionCentroMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
