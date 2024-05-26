b6b2dab03456e6a501955dbd58f6a79ce9592de1
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-25 20:38:37 -0400

    [Add] obtener consultorios medicos

--- src/main/java/com/example/microservicio_informacion_centro_medico/controller/ConsultoriosController.java
+++ src/main/java/com/example/microservicio_informacion_centro_medico/controller/ConsultoriosController.java
@@ -1,12 +1,29 @@
 package com.example.microservicio_informacion_centro_medico.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.microservicio_informacion_centro_medico.model.ConsultoriosEntity;
+import com.example.microservicio_informacion_centro_medico.repository.ConsultoriosRepositoryJPA;
+
 
 @Controller
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/consultorios")
 public class ConsultoriosController {
+
+    @Autowired
+    ConsultoriosRepositoryJPA consultoriosRepositoryJPA;
+
+    @GetMapping()
+    public List<ConsultoriosEntity> getConsultorios() {
+        return consultoriosRepositoryJPA.findAll();
+    }
     
 }
--- src/main/java/com/example/microservicio_informacion_centro_medico/model/ConsultoriosEntity.java
+++ src/main/java/com/example/microservicio_informacion_centro_medico/model/ConsultoriosEntity.java
@@ -1,5 +1,29 @@
 package com.example.microservicio_informacion_centro_medico.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "consultorios")
 public class ConsultoriosEntity {
-    
+    @Id
+    @Column(name = "id_consultorio")
+    private int id_consultorio;
+    @Column(name = "nombre")
+    private int nombre;
+    @Column(name = "direccion")
+    private int direccion;
+    @Column(name = "equipamiento")
+    private int equipamiento;
+    @Column(name = "id_especialidad")
+    private int idEspecialidad;
 }
