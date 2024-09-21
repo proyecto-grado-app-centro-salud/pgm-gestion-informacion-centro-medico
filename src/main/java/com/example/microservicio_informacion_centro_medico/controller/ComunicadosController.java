cb9ffdc848b418adeb8c6834a77af34ce0a2c0a0
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-09-21 16:34:54 -0400

    [Add] aniadiendo peticiones de comunicados

---
+++ src/main/java/com/example/microservicio_informacion_centro_medico/controller/ComunicadosController.java
@@ -0,0 +1,86 @@
+package com.example.microservicio_informacion_centro_medico.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.microservicio_informacion_centro_medico.model.dtos.ComunicadoDto;
+import com.example.microservicio_informacion_centro_medico.services.ComunicadosService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping()
+public class ComunicadosController {
+
+    @Autowired
+    private ComunicadosService comunicadosService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping(value = "/v1.0/comunicados")
+    public ResponseEntity<List<ComunicadoDto>> getComunicados() {
+        try {
+            return new ResponseEntity<>(comunicadosService.obtenerComunicados(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(value = "/v1.0/comunicados/{id}")
+    public ResponseEntity<ComunicadoDto> getComunicadoById(@PathVariable int id) {
+        try {
+            ComunicadoDto comunicadoDto = comunicadosService.obtenerComunicadoPorId(id);
+            return new ResponseEntity<>(comunicadoDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(value = "/v1.0/comunicados")
+    public ResponseEntity<ComunicadoDto> createComunicado(@RequestParam("data") String data,@RequestParam Map<String, MultipartFile> allFiles) {
+        try {
+            ComunicadoDto comunicadoDto = objectMapper.readValue(data, ComunicadoDto.class);
+            ComunicadoDto createdComunicado = comunicadosService.crearComunicado(comunicadoDto,allFiles);
+            return new ResponseEntity<>(createdComunicado, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/v1.0/comunicados/{id}")
+    public ResponseEntity<ComunicadoDto> updateComunicado(@PathVariable int id, @RequestParam("data") String data,
+    @RequestParam Map<String, MultipartFile> allFiles,@RequestParam Map<String, String> params) {
+        try {
+            ComunicadoDto comunicadoDto = objectMapper.readValue(data, ComunicadoDto.class);
+            ComunicadoDto updatedComunicado = comunicadosService.actualizarComunicado(id, comunicadoDto,allFiles,params);
+            return new ResponseEntity<>(updatedComunicado, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping(value = "/v1.0/comunicados/{id}")
+    public ResponseEntity<Void> deleteComunicado(@PathVariable int id) {
+        try {
+            comunicadosService.eliminarComunicado(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/com/example/microservicio_informacion_centro_medico/services/ComunicadosService.java
@@ -0,0 +1,87 @@
+package com.example.microservicio_informacion_centro_medico.services;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.microservicio_informacion_centro_medico.model.ComunicadoEntity;
+import com.example.microservicio_informacion_centro_medico.model.EspecialidadesEntity;
+import com.example.microservicio_informacion_centro_medico.model.dtos.ComunicadoDto;
+import com.example.microservicio_informacion_centro_medico.model.dtos.ConsultorioDto;
+import com.example.microservicio_informacion_centro_medico.repository.ComunicadosRepositoryJPA;
+import com.example.microservicio_informacion_centro_medico.repository.EspecialidadesRepositoryJPA;
+
+
+@Service
+public class ComunicadosService {
+
+    @Autowired
+    private ComunicadosRepositoryJPA comunicadosRepositoryJPA;
+
+    @Autowired
+    ImagenesService imagenesService;
+
+    @Autowired
+    EspecialidadesRepositoryJPA especialidadesRepositoryJPA;
+
+    public List<ComunicadoDto> obtenerComunicados() {
+        List<ComunicadoEntity> comunicadosEntities = comunicadosRepositoryJPA.findAllByDeletedAtIsNull();
+        List<ComunicadoDto> comunicadosDtos = new ArrayList<>();
+        for (ComunicadoEntity comunicadoEntity : comunicadosEntities) {
+            ComunicadoDto comunicadoDto = new ComunicadoDto().convertirComunicadoEntityAComunicadoDto(comunicadoEntity);
+            comunicadoDto.setImagenes(imagenesService.obtenerImagenes("comunicados", comunicadoEntity.getIdComunicado()));
+            comunicadosDtos.add(comunicadoDto);
+        }
+        return comunicadosDtos;
+    }
+
+    public ComunicadoDto obtenerComunicadoPorId(int id) {
+        ComunicadoEntity comunicadoEntity = comunicadosRepositoryJPA.findByIdComunicadoAndDeletedAtIsNull(id)
+            .orElseThrow(() -> new RuntimeException("Comunicado no encontrado"));
+        ComunicadoDto comunicadoDto = new ComunicadoDto().convertirComunicadoEntityAComunicadoDto(comunicadoEntity);
+        comunicadoDto.setImagenes(imagenesService.obtenerImagenes("comunicados", comunicadoEntity.getIdComunicado()));
+        return comunicadoDto;
+    }
+
+    public ComunicadoDto crearComunicado(ComunicadoDto comunicadoDto,Map<String,MultipartFile> allFiles) {
+        ComunicadoEntity comunicadoEntity = new ComunicadoEntity();
+        comunicadoEntity.setTitulo(comunicadoDto.getTitulo());
+        comunicadoEntity.setLugar(comunicadoDto.getLugar());
+        comunicadoEntity.setIntroduccion(comunicadoDto.getIntroduccion());
+        comunicadoEntity.setCuerpo(comunicadoDto.getCuerpo());
+        comunicadoEntity.setCitas(comunicadoDto.getCitas());
+        comunicadoEntity.setDatosContacto(comunicadoDto.getDatosContacto());
+        ComunicadoEntity savedEntity = comunicadosRepositoryJPA.save(comunicadoEntity);
+        List<MultipartFile> imagenes=imagenesService.obtenerImagenesDeArchivos(allFiles);
+        imagenesService.guardarImagenes(imagenes, "comunicados", savedEntity.getIdComunicado());
+        return new ComunicadoDto().convertirComunicadoEntityAComunicadoDto(savedEntity);
+    }
+
+    public ComunicadoDto actualizarComunicado(int id, ComunicadoDto comunicadoDto,Map<String, MultipartFile> allFiles,Map<String, String> params) {
+        ComunicadoEntity comunicadoEntity = comunicadosRepositoryJPA.findByIdComunicadoAndDeletedAtIsNull(id)
+            .orElseThrow(() -> new RuntimeException("Comunicado no encontrado"));
+
+        comunicadoEntity.setTitulo(comunicadoDto.getTitulo());
+        comunicadoEntity.setLugar(comunicadoDto.getLugar());
+        comunicadoEntity.setIntroduccion(comunicadoDto.getIntroduccion());
+        comunicadoEntity.setCuerpo(comunicadoDto.getCuerpo());
+        comunicadoEntity.setCitas(comunicadoDto.getCitas());
+        comunicadoEntity.setDatosContacto(comunicadoDto.getDatosContacto());
+        ComunicadoEntity updatedEntity = comunicadosRepositoryJPA.save(comunicadoEntity);
+        imagenesService.actualizarImagenes(allFiles, params, "comunicados", comunicadoEntity.getIdComunicado());
+        return new ComunicadoDto().convertirComunicadoEntityAComunicadoDto(updatedEntity);
+    }
+
+    public void eliminarComunicado(int id) {
+        ComunicadoEntity comunicadoEntity = comunicadosRepositoryJPA.findByIdComunicadoAndDeletedAtIsNull(id)
+            .orElseThrow(() -> new RuntimeException("Comunicado no encontrado"));
+        comunicadoEntity.markAsDeleted();
+        comunicadosRepositoryJPA.save(comunicadoEntity);
+    }
+}
